90e268768aae616cbfe7634368031ee3f35b067d
Takshsri <aymar131221@gmail.com>
2026-05-28 22:38:59 +0530

    feat: register task management routes

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from fastapi import FastAPI
 from models.model import Base
 from database import engine
 from routes.user_routes import router
-
+from routes.task_routes import router as task_router
 
 app = FastAPI()
 app.include_router(router)
-
+app.include_router(task_router)
 Base.metadata.create_all(bind=engine)
 @app.get("/")
 def home():
